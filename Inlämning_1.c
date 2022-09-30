1ecd8303ede51cbb6c7d536652ba2b0165a4e83d
IsaacKyh <isaac.leanderz@student.kyh.se>
2022-09-30 12:36:54 +0200

    Turn in

--- Inlämning_1.c
+++ Inlämning_1.c
@@ -5,20 +5,21 @@
 
 int main()
 {
-
+    // Variables
+    const int CM_CONVERTER = 100;
     float inputRadie, centimeterRadie, diameter, circumference, area;
-    // float area;
-    // float omkrets;
-    // float diameter;
-    // float Radie;
+
+    // Input
     printf("Enter a radius (in meters): ");
     scanf("%f", &inputRadie);
 
-    centimeterRadie = inputRadie * 100;
+    // Calculations
+    centimeterRadie = inputRadie * CM_CONVERTER;
     diameter = centimeterRadie * 2;
     circumference = 2 * M_PI * centimeterRadie;
     area = M_PI * pow(inputRadie, 2);
 
+    // Outputs
     printf("\nDiameter in cm is: %f", diameter);
 
     printf("\nThe circumference in cm is: %f", circumference);
